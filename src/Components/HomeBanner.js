e0ca668c53ea3c199803a6e0945f63b3617193eb
Ben Fawley <benfawley0@gmail.com>
2023-03-05 16:39:36 +0000

    Fix modal bug

--- src/Components/HomeBanner.js
+++ src/Components/HomeBanner.js
@@ -1,9 +1,9 @@
 import styles from "./HomeBanner.module.css";
 import movieBanner from "../Images/movie.jpg";
 
-const HomeBanner = () => {
+const HomeBanner = ({ innerRef }) => {
   return (
-    <div className={styles.banner}>
+    <div className={styles.banner} ref={innerRef}>
       <img src={movieBanner} alt="Movie Banner" />
       <div className={styles.bannerText}>
         <span>Welcome to</span>
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,29 +1,28 @@
-import React, { useContext } from 'react';
-import ReactDOM from 'react-dom';
-import MovieContext from '../Context/movie-context';
-import styles from './Modal.module.css';
-
+import React, { useContext } from "react";
+import ReactDOM from "react-dom";
+import MovieContext from "../Context/movie-context";
+import styles from "./Modal.module.css";
 
 const Backdrop = () => {
   const ctx = useContext(MovieContext);
 
-  return(
-    <div className={styles.backdrop} onClick={ctx.onCloseTrailer}></div>
-  )
-}
+  return <div className={styles.backdrop} onClick={ctx.onCloseTrailer}></div>;
+};
 
 const ModalOverlay = ({ src, title }) => {
   return (
-      <div className={styles.modal}>
-        <iframe className={styles.trailerIframe} src={`https://www.youtube.com/embed/${src}`} title={title} allowFullScreen>
-
-        </iframe>
-      </div>
-  )
-}
+    <div className={styles.modal}>
+      <iframe
+        className={styles.trailerIframe}
+        src={`https://www.youtube.com/embed/${src}`}
+        title={title}
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
 
 const Modal = ({ src }) => {
-
   return (
     <>
       {ReactDOM.createPortal(
@@ -35,7 +34,7 @@ const Modal = ({ src }) => {
         document.getElementById("overlay-root")
       )}
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
